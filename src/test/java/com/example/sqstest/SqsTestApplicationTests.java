b53a532a0216fbef6ab43077694ae8c5351591c8
Oleh Kuzmenko <okuzmenko@ascendix.com>
2021-09-21 10:43:00 +0300

    first commit

---
+++ src/test/java/com/example/sqstest/SqsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
