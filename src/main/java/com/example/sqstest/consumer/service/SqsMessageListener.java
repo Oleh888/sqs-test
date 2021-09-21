86c4f80ee5b4da5eb16ab4d95397fa6f4d23d300
Oleh Kuzmenko <okuzmenko@ascendix.com>
2021-09-21 18:20:11 +0300

    configure message listener

--- src/main/java/com/example/sqstest/consumer/service/SqsMessageListener.java
+++ src/main/java/com/example/sqstest/consumer/service/SqsMessageListener.java
@@ -1,10 +1,14 @@
 package com.example.sqstest.consumer.service;
 
+import com.alibaba.fastjson.JSON;
+import com.example.sqstest.dto.SimpleMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 @Slf4j
 @Service
@@ -12,6 +16,13 @@ public class SqsMessageListener implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
-
+        TextMessage textMessage = (TextMessage) message;
+        SimpleMessage simpleMessage = null;
+        try {
+            simpleMessage = JSON.parseObject(textMessage.getText(), SimpleMessage.class);
+        } catch (JMSException e) {
+            log.error("Json parse exception: {}", e.getMessage());
+        }
+        log.info("Simple message was got: {}", simpleMessage.toString());
     }
 }
